f2ea2ac3717270895525d7e385472108202f1a10
PawelSkrodzki <pawel.skrodzki14@gmial.com>
2022-11-16 17:39:25 +0100

    review fixes

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ showTime = () => {
   dateSpan.innerHTML = `${moment().format('DD/MM')}`;
 };
 setInterval(showTime, 1000);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.getElementById('slider');
+  let i = 0;
+  setInterval(() => {
+    slider.src = `img/slider${i}.jpg`;
+    i += 1;
+    if (i === 3) {
+      i = 0;
+    }
+  }, 2000);
+});
